8b62bf63b7c8ffc0dbf18a691cf6d434f6d508cb
alberington <bertval46@gmail.com>
2021-08-13 13:40:12 +0700

    update modul container

--- src/router/container/ContainerRouter.js
+++ src/router/container/ContainerRouter.js
@@ -4,7 +4,7 @@ const express = require("express");
 const routers = express.Router();
 
 // const ContainerController = require("../../modules/container/ContainerController");
-const { container_code: ContainerController } = require("../../modules");
+const { container: ContainerController } = require("../../modules");
 
 const Authentication = require("../../utils/middleware/Auth");
 const schemaValidation = require("../../utils/middleware/SchemaValidator");
@@ -16,6 +16,7 @@ routers.route("/list").get(Authentication, ContainerController.list);
 routers.route("/listone").get(Authentication, ContainerController.listOne);
 routers.route("/delete").delete(Authentication, ContainerController.delete);
 routers.route("/cek").post(Authentication, ContainerController.cek);
+routers.route("/checkcCode").get(Authentication, ContainerController.checkContainerCode);
 
 
 
