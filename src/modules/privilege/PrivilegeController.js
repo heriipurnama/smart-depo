2408f967e172c218d09af09376410ae7f3e6fee5
alberington <bertval46@gmail.com>
2021-08-30 12:01:11 +0700

    Fixing controller privilege

--- src/modules/privilege/PrivilegeController.js
+++ src/modules/privilege/PrivilegeController.js
@@ -12,10 +12,6 @@ class PrivilegeController {
 		let { group_id, module_id, has_insert, has_update, has_delete, has_approval, has_view,printpdf } = req.body;
 		try {
 			const payload = await privilege.create({ 
-				where: {
-					module_id: module_id
-				},
-				defaults: {
 					group_id: group_id,
 					module_id: module_id,
 					has_insert: has_insert,
@@ -25,7 +21,6 @@ class PrivilegeController {
 					has_view: has_view,
 					has_printpdf: printpdf,
 					has_printxls: printxls
-				}
 			})
 			if(!payload){
                 throw new Error(`Create Privilege Failed`);
