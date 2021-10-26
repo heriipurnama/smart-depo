d4897a379651166932e0c40a6906bd704c2ff31f
alberington <bertval46@gmail.com>
2021-10-26 22:04:53 +0700

    update query Eor Cost

--- src/modules/eor_cost/EorCostController.js
+++ src/modules/eor_cost/EorCostController.js
@@ -8,15 +8,18 @@ const Logger = require("../../utils/helper/logger");
 class EorCostController {
 
 	static async list(req, res, next) {
-		let {start, rows} = req.body;
+        let {limit, offset} = req.body;
+        let lmt = (limit == ""?``:` LIMIT ${limit}`) ;
+        let ofs = (offset == ""?``:` OFFSET ${offset}`) ;
 		try {
             let datas = await container_repair.sequelize.query(`SELECT SUM(rd.RDTOTALA) AS TOTAL
-                FROM container_repair rp
-                INNER JOIN container_repair_detail rd ON rp.SVID = rd.SVID and rp.RPVER=rd.RDNO
-                INNER JOIN tblrepair_method rm ON rd.RDREPMTD  = rm.RMCODE  
-                INNER JOIN container_survey sv ON sv.SVID = rp.SVID
-                INNER JOIN tblcurrency curr ON rd.RDCURR = curr.tucode
-                WHERE rp.RPCRNO = tblcontainer.CRNO and rd.SVID= container_repair.SVID and rd.RDNO= container_repair.RPVER and rd.RDAPP=1 and rd.RDCURR='002' 
+            FROM container_repair rp
+            INNER JOIN tblcontainer ctr ON ctr.crno = rp.rpcrno
+            INNER JOIN container_repair_detail rd ON rp.SVID = rd.SVID and rp.RPVER=rd.RDNO
+            INNER JOIN tblrepair_method rm ON rd.RDREPMTD  = rm.RMCODE  
+            INNER JOIN container_survey sv ON sv.SVID = rp.SVID
+            INNER JOIN tblcurrency curr ON rd.RDCURR = curr.tucode
+            WHERE rp.RPCRNO = ctr.CRNO and rd.SVID= rp.SVID and rd.RDNO= rp.RPVER and rd.RDAPP=1 and rd.RDCURR='002' ${lmt} ${ofs} 
                 `, 
                 {
                     type: container_repair.SELECT
