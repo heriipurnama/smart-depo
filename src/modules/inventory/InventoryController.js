c83b8c4ef99b2e67adc74c27eb834419da0da81a
alberington <bertval46@gmail.com>
2021-10-26 23:17:46 +0700

    fixing query

--- src/modules/inventory/InventoryController.js
+++ src/modules/inventory/InventoryController.js
@@ -8,17 +8,19 @@ const Logger = require("../../utils/helper/logger");
 class InventoryController {
 
 	static async list(req, res, next) {
-        let {prcode, clength, ctcode, condition, sdcode, start, rows} = req.body;
+        let {prcode, clength, ctcode, condition, sdcode, limit, offset} = req.body;
         let $sdcode = (sdcode=="")? `` : ` cp.cpdepo='`+sdcode+`' and`;
         let $prcode = (prcode=="")? `` : ` cp.cpopr='`+prcode+`' and`;
         let $clength = (clength=="")? `` : ((clength=="hc")? `(cc.ccheight > '9' or cc.cclength='45' ) and ` : `(cc.cclength='`+clength+`' and cc.ccheight < '9' ) and `);
         let $ctcode = (ctcode=="")? `` : ` cc.ctcode='`+ctcode+`' and`;
         let $condition = (condition=="")? `` : ` left(con.crlastcond,1) =left('`+condition+`',1) and`;
+        let $limit = (limit=="")? `` : ` limit ${limit}`;
+        let $offset = (offset=="")? `` : ` offset ${offset}`;
 
 		try {
-            let datas = await container_process.sequelize.query(`select distinct cpdepo, cpopr,  sdname,  dpname,
+            let datas = await container_process.sequelize.query(`select distinct cpdepo, cpopr, dpname,
                 cp.crno as container_no, cc.cccode as id_code, cc.ctcode as ctype, cc.cclength as clength, cc.ccheight as cheight,
-                con.mtcode1 as mat, vesid as ves, cpivoy as voy, date_format(cp.cpidisdat,'%d-%m-%y') as disch_date,
+                vesid as ves, cpivoy as voy, date_format(cp.cpidisdat,'%d-%m-%y') as disch_date,
                 datediff(now(), cp.cpidisdat) + 1 as dlq, date_format(cp.cpitgl,'%d-%m-%y') as date_in, 
                 datediff(now(), cp.cpitgl) + 1 as days,
                 con.crlastcond as status, cp.cpiremark as remarks,
@@ -28,7 +30,6 @@ class InventoryController {
                 left join order_container_repo cr on cr.reorderno=cp.cpiorderno
                 left join tblcontainer_code cc on cc.cccode = con.cccode 
                 left join tblvoyage voy on voy.voyid = cp.cpivoyid
-                left join tblsubdepo s on s.sdcode= cp.spdepo
                 left join tbldepo d on d.dpcode= cp.cpdepo
             where 
                 ${$sdcode} 
@@ -39,7 +40,8 @@ class InventoryController {
                 (cp.cpotgl is null or cp.cpotgl='0000-00-00') 
                 and con.crlastact<>'OD' and con.crlastact<>'BI' 
                 and cp.crno is not null
-            order by cpopr, sdname, id_code, cp.cpitgl, con.crlastcond`, 
+            order by cpopr, dpname, id_code, days, con.crlastcond
+            ${$limit} ${$offset}`, 
             {
                 type: container_process.SELECT
             }
