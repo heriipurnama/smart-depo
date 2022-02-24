8e386e343bf43127ce03d4433521e2ee401c2d03
heripurnama <heri.purnama@xapiens.id>
2022-02-24 18:23:11 +0700

    add new smptp

--- src/utils/services/ServiceEmail.js
+++ src/utils/services/ServiceEmail.js
@@ -6,36 +6,35 @@ require("dotenv").config();
 
 const ServiceEmail = (email, usernameEncript) => {
 	try {
-
 		let transport = nodemailer.createTransport({
-			service: "gmail",
+			service: "Outlook365",
 			secure: false,
-			port: 25,
+			port: 587,
 			auth: {
 				// eslint-disable-next-line no-undef
 				user: process.env.SERVER_EMAIL_USER,
-				pass: process.env.SERVER_EMAIL_PASS
-			}, tls: {
-				rejectUnauthorized: false
-			}
+				pass: process.env.SERVER_EMAIL_PASS,
+			},
+			tls: {
+				rejectUnauthorized: false,
+			},
 		});
-      
+
 		const mailOptions = {
 			from: process.env.SERVER_EMAIL_USER,
 			to: email,
 			subject: "Please activate your account",
-			text: `Please click the following link to verify your email: ${process.env.BASE_URL}/api/v1/users/auth/activate?alg_wc_ev_verify_email=${usernameEncript}`
+			text: `Please click the following link to verify your email: ${process.env.BASE_URL}/api/v1/users/auth/activate?alg_wc_ev_verify_email=${usernameEncript}`,
 		};
-      
-		transport.sendMail(mailOptions, function(error, info){
+
+		transport.sendMail(mailOptions, function (error, info) {
 			if (error) {
 				console.log(error);
 			} else {
 				console.log("Email send: " + info.response);
 			}
 		});
-	}
-	catch (error) {
+	} catch (error) {
 		console.log(error);
 	}
 };
